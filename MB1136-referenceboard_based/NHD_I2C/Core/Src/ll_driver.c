7037eda819645da3da2e0379dbbf181b35bfc18e
Acefrrag <59066474+Acefrrag@users.noreply.github.com>
2025-06-01 01:45:26 +0200

    Moved NHD project in internal project directory

---
+++ MB1136-referenceboard_based/NHD_I2C/Core/Src/ll_driver.c
@@ -0,0 +1,8 @@
+/*
+ * ll_driver.c
+ *
+ *  Created on: May 10, 2025
+ *      Author: Michele Pio Fragasso
+ */
+
+#include "ll_driver.h"
---
+++ MB1136-referenceboard_based/NHD_I2C/Debug/Core/Src/ll_driver.d
@@ -0,0 +1,2 @@
+Core/Src/ll_driver.o: ../Core/Src/ll_driver.c ../Core/Inc/ll_driver.h
+../Core/Inc/ll_driver.h:
